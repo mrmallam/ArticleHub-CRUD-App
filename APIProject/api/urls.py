e8d0106e2a85964d4919aae25f696c3ff192f52b
mrmallam <96600566+mrmallam@users.noreply.github.com>
2023-02-10 19:32:46 -0700

    Class based api views

--- APIProject/api/urls.py
+++ APIProject/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import article_list 
+from .views import ArticleList
+
 
 urlpatterns = [
-    path('articles/', article_list),
-] 
+    path('articles/', ArticleList.as_view()) # we use as_view because we're using class based api views
+]
+
+
+
+# # article_list, article_details
+# urlpatterns = [
+#     path('articles/', article_list),
+#     path('articles/<int:pk>/', article_details)
+# ] 
--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -3,12 +3,32 @@ from .models import Article
 from .serializers import ArticleSerializer
 from django.http import JsonResponse 
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view # for functional based api View decorators
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import APIView # this for class based api views decorators
 
 # Create your views here.
 
 
-@csrf_exempt # per django documentation... need to be added from the POST method
+class ArticleList(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+'''
+@api_view(['GET', 'POST']) # for the browsable API
 def article_list(request):
 
     # if GET request
@@ -19,20 +39,40 @@ def article_list(request):
         articles = Article.objects.all()
         # now we need to serialize the 'articles' query set. if you want to serialize a query set then you need to add "many=True"
         serializer = ArticleSerializer(articles, many=True)
-
-        # now we just need to return a JSON response
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     
     # else if POST request
     elif request.method == 'POST':
-        # we need to parse the POST request
-        data = JSONParser().parse(request)
-        # create our serializer 
-        serializer = ArticleSerializer(data=data)
+
+        serializer = ArticleSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.error, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_details(request, pk): # pk = primary key
+    try:
+        article = Article.objects.get(pk=pk)
     
+    except Article.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        # create the serializer
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT": # update
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == "DELETE":
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
