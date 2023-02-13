9b6a7ac400b5e4919ecae20d43d217736cd1d0db
mrmallam <96600566+mrmallam@users.noreply.github.com>
2023-02-12 19:07:01 -0700

    Backend Completed

--- APIProject/api/urls.py
+++ APIProject/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ArticleViewSet
+from .views import ArticleViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
 # from .views import ArticleList, ArticleDetails
 
@@ -7,10 +7,12 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('articles', ArticleViewSet, basename='atricles')
 
+router.register('users', UserViewSet)
+
 urlpatterns = [
     # path('articles/', ArticleList.as_view()), # we use as_view because we're using class based api views
     # path('articles/<int:id>/', ArticleDetails.as_view()),   
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
 
 
--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import Article
-from .serializers import ArticleSerializer
+from .serializers import ArticleSerializer, UserSerializer
 from django.http import JsonResponse 
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view # for functional based api View decorators
@@ -11,7 +11,9 @@ from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -19,6 +21,16 @@ from django.shortcuts import get_object_or_404
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication, )
+    
+    
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 
 
@@ -34,8 +46,6 @@ class ArticleViewSet(viewsets.ModelViewSet):
     
     
     
-    
-
 
 
 
