fc69c52275131ecf11ac912b08c807be48feb924
mrmallam <96600566+mrmallam@users.noreply.github.com>
2023-02-13 21:36:11 -0700

    fixes

--- APIProject/api/urls.py
+++ APIProject/api/urls.py
@@ -6,19 +6,8 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('articles', ArticleViewSet, basename='atricles')
-
 router.register('users', UserViewSet)
 
 urlpatterns = [
-    # path('articles/', ArticleList.as_view()), # we use as_view because we're using class based api views
-    # path('articles/<int:id>/', ArticleDetails.as_view()),   
     path('api/', include(router.urls)),
 ]
-
-
-
-# # article_list, article_details
-# urlpatterns = [
-#     path('articles/', article_list),
-#     path('articles/<int:pk>/', article_details)
-# ] 
