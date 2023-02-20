e126f86bd480b74ce3e8fb89c3f8f0f7b6d07f9e
mrmallam <96600566+mrmallam@users.noreply.github.com>
2023-02-19 17:53:31 -0700

    changing name to 'backend'

--- practive_django_rest_framework/practice_drf/one_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Person
-
-# Register your models here.
-
-@admin.register(Person)
-class ArticleModel(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'age')
-    list_filter = ('first_name', 'last_name', 'age')
--- practive_django_rest_framework/practice_drf/one_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OneAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'one_app'
